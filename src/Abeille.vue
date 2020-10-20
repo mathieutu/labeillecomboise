42475df433c1288195624bd4c7718a68ee7c85c3
zalem134 <72811323+zalem134@users.noreply.github.com>
2020-10-20 14:34:20 +0200

    Update Abeille.vue

--- src/Abeille.vue
+++ src/Abeille.vue
@@ -21,7 +21,7 @@
         </div>
         <div class="flex items-center w-full p-6 sm:w-1/2">
           <img class="object-cover w-full mx-auto rounded-lg shadow-lg sm:h-64 " 
-          src="public/img/apiculteur.jpg " 
+          src="src/apiculteur.jpg " 
           alt="photo apiculteur">
         </div>
       </div>
